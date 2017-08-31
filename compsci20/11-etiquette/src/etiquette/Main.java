133ab23fbc4ff8eee58fd94bffcf04be739744e7
Peyton Seigo <pseigo@yahoo.com>
2017-08-30 23:11:13 -0600

    Add line break after first input

--- compsci20/11-etiquette/src/etiquette/Main.java
+++ compsci20/11-etiquette/src/etiquette/Main.java
@@ -6,29 +6,30 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		// get # of seats
 		System.out.println("How many seats are in the row?");
 		int seats = read(in, 1);
 		in.nextLine(); // flush stream
-		
+        System.out.println();
+
 		boolean[] occupied = new boolean[seats];
-		
+
 		// get state of seats
 		for (int i = 0; i != seats; ++i) {
 			System.out.println("Is seat " + (i+1)
 				+ " free? (Y/N)?");
-			
+
 			occupied[i] = read(in, "N", "Y");
 			System.out.println();
 		}
-		
+
 		in.close();
 
 		// initialize row and get seat
-		Row row = new Row(occupied);		
+		Row row = new Row(occupied);
 		int seat = row.getFarthestSeat();
-		
+
 		if (seat == -1) {
 			row.println();
 			System.out.println("There are no seats available!");
@@ -37,7 +38,7 @@ public class Main {
 			System.out.println("You should use seat " + (seat + 1) + "!");
 		}
 	}
-	
+
     public static int read(Scanner in, int min) {
         int input;
 
@@ -59,20 +60,20 @@ public class Main {
 
         return input;
     }
-	
+
     public static boolean read(Scanner in, String accept, String decline) {
         String input;
-        
+
         do {
             try {
             	System.out.print("> ");
                 input = in.nextLine();
-                
+
                 input = input.toLowerCase();
-                
+
                 if (!input.equals(accept.toLowerCase())
                 		&& !input.equals(decline.toLowerCase()))
-                	
+
                     throw new InputMismatchException();
                 break;
             } catch (InputMismatchException e) {
