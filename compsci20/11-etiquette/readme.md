f328b6ce24c724a682e8eb068ac6cd29c4f7c514
Peyton Seigo <pseigo@yahoo.com>
2017-08-30 22:36:44 -0600

    Add etiquette assignment 11

---
+++ compsci20/11-etiquette/readme.md
@@ -0,0 +1,9 @@
+# Notes
+applicable to many situations
+
+# Instructions
+Many people do not know this, but when a person rides a bus, they shouldn't just choose a seat at random. Instead they should follow the rules of etiquette that promote comfort and cleanliness for all involved.
+
+Now, the rules of transit etiquette are quite simple — always choose the seat that is furthest away from the most people and (in the case of a tie) closest to the ends. That's it.
+
+Please make a program that will ask the user to tell you how many seats are in the row. Then the program will ask you if each one is occupied or unoccupied. After doing this, the program will tell you which one you should go to if you want to be polite.
---
+++ compsci20/11-etiquette/src/etiquette/Main.java
@@ -0,0 +1,86 @@
+package etiquette;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		// get # of seats
+		System.out.println("How many seats are in the row?");
+		int seats = read(in, 1);
+		in.nextLine(); // flush stream
+		
+		boolean[] occupied = new boolean[seats];
+		
+		// get state of seats
+		for (int i = 0; i != seats; ++i) {
+			System.out.println("Is seat " + (i+1)
+				+ " free? (Y/N)?");
+			
+			occupied[i] = read(in, "N", "Y");
+			System.out.println();
+		}
+		
+		in.close();
+
+		// initialize row and get seat
+		Row row = new Row(occupied);		
+		int seat = row.getFarthestSeat();
+		
+		if (seat == -1) {
+			row.println();
+			System.out.println("There are no seats available!");
+		} else {
+			row.println('^', seat);
+			System.out.println("You should use seat " + (seat + 1) + "!");
+		}
+	}
+	
+    public static int read(Scanner in, int min) {
+        int input;
+
+        do {
+            try {
+            	System.out.print("> ");
+                input = in.nextInt();
+                if (input < min)
+                    throw new InputMismatchException();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: must enter an integer "
+                	+ "greater than or equal to " + min + ".\n");
+
+                // clear bad input
+                in.nextLine();
+            }
+        } while (true);
+
+        return input;
+    }
+	
+    public static boolean read(Scanner in, String accept, String decline) {
+        String input;
+        
+        do {
+            try {
+            	System.out.print("> ");
+                input = in.nextLine();
+                
+                input = input.toLowerCase();
+                
+                if (!input.equals(accept.toLowerCase())
+                		&& !input.equals(decline.toLowerCase()))
+                	
+                    throw new InputMismatchException();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: must input "
+                    + accept + " or " + decline + ".\n");
+            }
+        } while (true);
+
+        return input.equals(accept.toLowerCase());
+    }
+}
