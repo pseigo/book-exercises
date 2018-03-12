c815392879fdd7df1a9921a2617ccb6f4870f351
Peyton Seigo <pseigo@yahoo.com>
2018-03-11 21:46:39 -0600

    Minor refactoring to fib.exs

--- learn-elixir-tv/practice/fib/fib.exs
+++ learn-elixir-tv/practice/fib/fib.exs
@@ -1,19 +1,18 @@
 defmodule Backtrack do
   @moduledoc """
-  Contains the `fib/1` function used to calculate a given term in the fibonacci
-  series. This solution uses backtracking; therefore, it is not tail-call
-  optimized.
+  Contains fib/1, used to calculate a given term in the fibonacci series. This
+  solution uses backtracking; therefore, it is not tail-call optimized.
 
   # Performance issues
 
   For some reason, this solution is incredibly slow. On my machine, it takes
-  around 5 seconds to calculate `fib(40)`, whereas `TailCall.fib/1 can
-  calculate a huge term such as fib(10000) instantly.
+  around 5 seconds to calculate `fib(40)`, whereas TailCall.fib/1 can
+  calculate a huge term such as `fib(10000)` instantly.
   """
   def fib(0), do: 0
   def fib(1), do: 1
 
-  def fib(term) when term >=0 do
+  def fib(term) when term >=0 and is_integer(term) do
     fib(term - 2) + fib(term - 1)
   end
 
@@ -22,24 +21,24 @@ end
 
 defmodule TailCall do
   @moduledoc """
-  Contains the `fib/1` function used to calculate a given term in the fibonacci
-  series. This solution is tail call optimized.
+  Contains fib/1, used to calculate a given term in the fibonacci series. This
+  solution is tail call optimized.
   """
 
   def fib(0), do: 0
   def fib(1), do: 1
 
-  def fib(term) when term >= 0 do
-    do_fib(2, term, {fib(0), fib(1)})
+  def fib(term) when term >= 0 and is_integer(term) do
+    do_fib({fib(0), fib(1)}, 2, term)
   end
 
   def fib(_), do: {:error, "fibonacci term must be 0 or greater"}
 
-  defp do_fib(curr, term, {prev2, prev1}) when curr == term do
+  defp do_fib({prev2, prev1}, term, target) when term == target do
     prev2 + prev1
   end
 
-  defp do_fib(curr, term, {prev2, prev1}) do
-    do_fib(curr + 1, term, {prev1, prev2 + prev1})
+  defp do_fib({prev2, prev1}, term, target) do
+    do_fib({prev1, prev2 + prev1}, term + 1, target)
   end
 end
