e1b3363379155dd89336ba8b1e0fadf5f2a8b810
Peyton Seigo <pseigo@yahoo.com>
2018-03-22 01:01:35 -0600

    Continue episode 10 of learn-elixir-tv

--- learn-elixir-tv/episodes/10-mix/crypto/config/config.exs
+++ learn-elixir-tv/episodes/10-mix/crypto/config/config.exs
@@ -2,7 +2,9 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# config :atom, keyword: "list"
 config :crypto, prefix: "Hello: "
+config :crypto, Crypto.Encryptor, prefix: "Encrypted: "
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
