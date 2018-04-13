d9206dbffb06c1372098c94c05314043ceb48d82
Peyton Seigo <pseigo@yahoo.com>
2018-04-12 20:44:12 -0600

    Delete calculus project (migrated to pseigo/leibniz)

--- learn-elixir-tv/practice/calculus/test/calculus/differential_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule Calculus.DifferentialTest do
-  use ExUnit.Case
-  doctest Calculus.Differential
-end
--- learn-elixir-tv/practice/calculus/test/calculus_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CalculusTest do
-  use ExUnit.Case
-  doctest Calculus
-
-  test "greets the world" do
-    assert Calculus.hello() == :world
-  end
-end
